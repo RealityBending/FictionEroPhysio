1ac030daeb90781f768722f45b77b7711af7a794
Róisín Sharma <rs843@sussex.ac.uk>
2025-12-01 19:39:59 +0000

    Update demographics.js
    changed to anas version

--- experiment/demographics.js
+++ experiment/demographics.js
@@ -2,61 +2,98 @@
 
 const demographics_consent = {
     type: jsPsychSurvey,
-    survey_json: {
-        showQuestionNumbers: false,
-        completeText: "I read, understood, and I consent",
-        pages: [
-            {
-                elements: [
-                    {
-                        type: "html",
-                        name: "consent_form",
-                        html: `
+    survey_json: function () {
+        return {
+            showQuestionNumbers: false,
+            completeText: "Continue",
+            pages: [
+                {
+                    elements: [
+                        // Info text first
+                        {
+                            type: "html",
+                            name: "information_sheet",
+                            html: `
+                    
+                    <img src='https://blogs.brighton.ac.uk/sussexwrites/files/2019/06/University-of-Sussex-logo-transparent.png' width='150px' align='right'/><br><br><br><br><br>
+
+                     <h1>Informed Sheet</h1>
+                    <p><b>Invitation to Take Part</b><br>
+                    Thank you for considering to take part in this study that aims at deepening our understanding of human perception. 
+                    This study is being conducted by Dr Dominique Makowski, and his team, from the <b>University of Sussex</b>, who are happy to be contacted if you have any questions (see contact information below).</p>
+
+                    <p><b>Why have I been invited and what will I do?</b><br>
+                    The goal is to study how new technology can impact <b>human perception</b>.
+                    In this study, you will be shown <b>erotic images</b> and asked to complete a few questionnaires and perform some tasks. 
+                    We will also measure your physiological processes to assess the relationship between how you experience your body and how you perceive these images.</p>
+                        
+                    <p><b>Do I have to take part?</b><br>
+                    Your participation is entirely voluntary. You are free to choose not to take part, or to withdraw at any stage without having to give a reason and without being penalised in any way.
+                    If you choose to take part, you will be asked to provide your consent electronically before starting the study.
+                    As we are not collecting any personally identifiable information, it will not be possible to withdraw your data after you submit it.
+                    
+                        <p><b>Physiological Signals</b><br>
+                    We will be recording some of your body's electric activity using ECG and EEG; both are simple non-intrusive procedures used in research and in the life sciences. 
+                    Since this study is being undertaken for research purposes only, we will not be able to provide any feedback or information on clinical aspects of the data obtained. 
+                    For the ECG we will be attaching some sensors to the skin, and for the EEG we will be using the Muse 2 headset which contains electrodes that will be placed on your forehead and behind the ears. 
+                    We kindly ask that you refrain from participating if you are aware of having skin reactions or allergies to adhesives or the materials used in the electrodes - primarily composed of silver and silver chloride.</p>
+
+                    <p><b>What will happen to the results and my personal information?</b><br>
+                    The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. 
+                    <b>Please read this information carefully</b> and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>
+                    
+                    <p><b>Who has approved this study?</b><br>
+                    This study has been approved by the Faculty Research Ethics Committee: Science, Engineering and Technology, and the project reference number is 0294.
+                    
+                    <p><b>Insurance</b><br>  
+                    The University of Sussex has insurance in place to cover its legal liabilities in respect of this study.
+
+                    <p><b>Contact for Further Information</b><br>  
+                    For further information about this research, or if you have any concerns, please contact Dr Dominique Makowski 
+                    (<i style='color:DodgerBlue;'>D.Makowski@sussex.ac.uk</i>), Ana Neves (<i style='color:DodgerBlue;'>A.Neves@sussex.ac.uk</i>).
+                    If you have any concerns about the way in which the study has been conducted, you should contact the SEMSET Faculty Research Ethics Committee at <i style='color:DodgerBlue;'>frecsemset@sussex.ac.uk</i>.
+                    
                 
-                <img src='https://blogs.brighton.ac.uk/sussexwrites/files/2019/06/University-of-Sussex-logo-transparent.png' width='150px' align='right'/><br><br><br><br><br>
-                <h1>Informed Consent</h1>
-                
-                <div class="narrow-text" style="text-align: left; max-width: 800px; margin: 0 auto;">
-                <p><b>Invitation to Take Part</b><br>
-                Thank you for considering to take part in this study that aims at deepening our understanding of human perception. 
-                This study is being conducted by Dr Dominique Makowski, and his team, from the <b>University of Sussex</b>, who are happy to be contacted if you have any questions (see contact information below).</p>
+                            <h1><b>Consent</h1>
 
-                <p><b>Why have I been invited and what will I do?</b><br>
-                The goal is to study how new technology can impact <b>human perception</b>.
-                In this study, you will be shown <b>erotic images</b> and asked to complete a few questionnaires and perform some tasks. 
-                We will also measure your physiological processes to assess the relationship between how you experience your body and how you perceive these images.</p>
-
-                <p><b>Physiological Signals</b><br>
-                We will be recording some of your body's electric activity using ECG and EEG; both are simple non-intrusive procedures used in research and in the life sciences. 
-                Since this study is being undertaken for research purposes only, we will not be able to provide any feedback or information on clinical aspects of the data obtained. 
-                For the ECG we will be attaching some sensors to the skin, and for the EEG we will be using the Muse headset which contains electrodes that will be placed on your forehead and behind the ears. 
-                We kindly ask that you refrain from participating if you are aware of having skin reactions or allergies to adhesives or the materials used in the electrodes - primarily composed of silver and silver chloride.</p>
-
-                <p><b>What will happen to the results and my personal information?</b><br>
-                The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. 
-                <b>Please read this information carefully</b> and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>
-
-                <p><b>Consent</b></p>
-                <ul style="padding-left: 20px;">
-                  <li>I understand that by clicking below I am agreeing to take part in the University of Sussex research described here, and that I have read and understood this information sheet.</li>
-                  <li>I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalized in any way.</li>
-                  <li>I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed it.</li>
-                  <li>I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.</li>
-                  <li>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publicly available through secured scientific online data repositories.</li>
-                  <li>I understand that due to the explicit nature of the images, <b>I confirm that I am 18 years of age or older</b>.</li>
-                </ul>
-
-                <p><sub>
-                For further information about this research, or if you have any concerns, please contact Dr Dominique Makowski 
-                (<i style='color:DodgerBlue;'>D.Makowski@sussex.ac.uk</i>), Ana Neves (<i style='color:DodgerBlue;'>A.Neves@sussex.ac.uk</i>).
-                This research has been approved (reference: 0294) by the ethics board of the School of Psychology. 
-                The University of Sussex has insurance in place to cover its legal liabilities in respect of this study.
-                </sub></p>
-                </div>`,
-                    },
-                ],
-            },
-        ],
+                            `,
+                        },
+                        // Consent checkboxes
+                        {
+                            type: "checkbox",
+                            name: "consent_1",
+                            title: "If you agree to participate in this study, then please read the following statements, and tick the box next to each statement to indicate consent.",
+                            choices: [
+                                "I understand that by checking this box I am agreeing to take part in the University of Sussex research described here, and that I have read and understood this information sheet.",
+                                "I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalized in any way.",
+                                "I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed it.",
+                                "I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.",
+                                "I understand that my collected data will be stored in a de-identified way. De-identified data may be made publicly available through secured scientific online data repositories.",
+                                "I understand that due to the explicit nature of the images, I confirm that I am 18 years of age or older.",
+                            ],
+                            isRequired: true,
+                            validators: [
+                                {
+                                    type: "answercount",
+                                    minCount: 6, // number of checkboxes in choices
+                                    maxCount: 6,
+                                    text: "You must agree to all statements before continuing.",
+                                },
+                            ],
+                        },
+                        {
+                            type: "checkbox",
+                            name: "consent_2",
+                            title: "By participating, you agree to follow the instructions and provide honest answers. If you do not wish to participate, simply close your browser.",
+                            choices: [
+                                "Please tick this box if you consent to taking part in this study:",
+                            ],
+                            isRequired: true,
+                        },
+                    ],
+                },
+            ],
+        }
     },
     data: { screen: "consent" },
 }
@@ -91,7 +128,12 @@ const demographics_questions1 = {
                         name: "Gender",
                         type: "radiogroup",
                         // Should change other to prefer to self-describe ? (for ethical approval)
-                        choices: ["Male", "Female", "Other"],
+                        choices: [
+                            "Male",
+                            "Female",
+                            "Other",
+                            "Prefer not to say",
+                        ],
                         isRequired: true,
                         colCount: 0,
                     },
@@ -352,6 +394,7 @@ const demographics_debriefing = {
                 We are testing the hypothesis that believing erotic images are fake leads to lower sexual arousal.</p>
                 <p>We are also assessing the role of <b style="color: #e228b1ff;">interoception</b> in this process. Interoception refers to the awareness of internal bodily states - both physiological (e.g., heart rate) and emotional - and it plays a crucial role in how we perceive and interpret the world around us.</p>
                 <p>Because we are interested in your <i>beliefs</i> about reality, <b>all the images you saw were actually real</b>. They were selected from a validated psychological database used in research on emotion.</p>
+                We also told you we were working with an <b>AI startup</b> company based in Brighton. This was also not true; <b>there is no such partnership or company</b>.
                 <p>We apologize for the necessary deception used in the task instructions. Deception was essential to maintain the validity of our experimental manipulation. We hope you understand the rationale behind this decision.</p>
 
                 <p><b>Thank you again!</b> Your participation in this study will be kept completely confidential. If you have any questions or concerns about the study, feel free to contact us:</p>
